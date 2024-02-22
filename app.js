88111d0b889f1697a50d4830010f6b98a5772b61
seonyo <jsy6535@naver.com>
2024-02-22 20:03:12 +0900

    corse 설정

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 const express = require('express')
-const mysql = require('mysql')
+const cors = require('cors');
+const app = express();
+const port = 3000;
 
-const app = express()
-
-app.use(express.json())
-
-const pool = mysql.createPool({
-    host : 'localhost',
-    user : 'root',  
-    password : '591006',
-    database : 'users'
-})
+app.use(express.json());
+app.use(cors());
 
 app.post('/users/inform', (req, res)=>{
 	let {name, student_no, phone_no, email} = req.body

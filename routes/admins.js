cb3a84f8fd026f711817e045cfda551b73f45b56
ezi-s-u <ljsgoo0617@gmail.com>
2024-02-25 23:54:11 +0900

    관리자 로그인 api  구현

--- routes/admins.js
+++ routes/admins.js
@@ -1,6 +1,19 @@
 const pool = require('../db/db');
 const router = require('express').Router();
 
+// 관리자 로그인 api
+router.post('/login', (req, res) => {
+    const { password } = req.body;
+
+    const real_password = "미베니들012사랑해"
+
+    if (password === real_password) {
+        return res.status(200).json({ message: '로그인 성공' });
+    } else {
+        return res.status(401).json({ message: '인증 실패' });
+    }
+});
+
 // 지원서 내용 모두 불러오는 api
 router.get('/', (req, res)=>{
 	pool.query('SELECT * FROM apply_info', (err, result) => {

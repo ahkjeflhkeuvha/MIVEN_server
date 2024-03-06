5a21d256017f0063e9052d92ab6ce1ab7d208224
ezi-s-u <ljsgoo0617@gmail.com>
2024-03-06 23:39:38 +0900

    로그인 api

--- db/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2');
-const pool = mysql.createPool({
-    host : 'localhost',
-    user : 'root',  
-    password : '990327',
-    database : 'miven_apply'
-})
-
-module.exports = pool;

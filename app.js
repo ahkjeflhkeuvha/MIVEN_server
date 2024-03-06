57893b1a4cdf2bb5dbbf43fd896329c0e5a976f5
seonyo <jsy6535@naver.com>
2024-03-07 08:30:46 +0900

    필요없는 코드 삭제

--- app.js
+++ app.js
@@ -13,31 +13,6 @@ app.use('/users', user);
 const admin = require('./routes/admins');
 app.use('/admins', admin);
 
-
-app.post('/users/save', (req, res)=>{
-    let {q_1, q_2, q_3} = req.body
-    pool.query('INSERT INTO apply_info (q_1, q_2, q_3) VALUES (?,?,?)', [q_1, q_2, q_3], (err, result)=>{
-        if(err){
-            console.log(err.message)
-            res.status(500).json({ message : '제출 실패' })
-        } else {
-            res.status(200).json({ message : '제출 성공' })
-        }
-    })
-})
-
-app.patch('/users/presave', (req, res)=>{
-    let {q_1, q_2, q_3} = req.body
-    pool.query('UPDATE apply_info SET q_1=?, q_2=?, q_3=?', [q_1, q_2, q_3], (err, result)=>{
-        if(err){
-            console.log(err.message)
-            res.status(500).json({ message : '임시저장 실패' })
-        } else {
-            res.status(200).json({ message : '임시저장 성공' })
-        }
-    })
-})
-
 app.listen(port, () => {
     console.log(`Example app listeing on port ${port}`)
 });
